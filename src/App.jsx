b35a40b4dfdaac21b221746a05884e473ad931e9
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-11 23:03:44 +0530

    Integrate App Layout and Router Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import AppLayout from './layouts/App.layout';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Category from './pages/Category';
+import Favorites from './pages/Favorites';
+import Search from './pages/Search';
+import SingleGif from './pages/SingleGif';
+import GifContextProvider from './context/GifContext';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/:category',
+        element: <Category />,
+      },
+      {
+        path: '/search/:query',
+        element: <Search />,
+      },
+      {
+        path: '/:type/:slug',
+        element: <SingleGif />,
+      },
+      {
+        path: '/favorites',
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <GifContextProvider>
+      <RouterProvider router={router} />
+    </GifContextProvider>
+  );
 };
 
 export default App;
